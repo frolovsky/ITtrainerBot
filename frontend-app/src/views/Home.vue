427d52ff5c39f43ce6015231a16708a62a619207
frolovsky <apf75@yandex.ru>
2021-11-15 10:42:22 +0300

    add empty frontend app

---
+++ frontend-app/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  name: 'Home'
+})
+export default class Home extends Vue {}
+</script>
