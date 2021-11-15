146f5364d4b1e7dd844d7317f8e8f4172020d4ea
frolovsky <apf75@yandex.ru>
2021-11-15 16:13:24 +0300

    backend: enable cors, add multer package for upload files

--- backend-app/app.js
+++ backend-app/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const questionRouter = require('./resources/questions/questions.router');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/questions', questionRouter);
 
